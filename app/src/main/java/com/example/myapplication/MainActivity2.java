9180389358d49f3b1f99a8bd5f5a114c42429153
KatieStacey <katiestacey44@gmail.com>
2023-11-21 09:06:28 -0500

    documemtation of main activity 2

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -10,7 +10,6 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -20,7 +19,10 @@ import androidx.core.content.ContextCompat;
 
 import com.google.android.material.textfield.TextInputEditText;
 
-
+/**
+ * MainActivity2 is the second activity in the Android Map Navigation App.
+ * It displays a map with a navigation path and room markers.
+ */
 public class MainActivity2 extends AppCompatActivity {
     private Context context;
     private Resources resources;
@@ -31,72 +33,86 @@ public class MainActivity2 extends AppCompatActivity {
     private float scaleValValue;
     private Path path;
 
-
-
+    /**
+     * Called when the activity is first created.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being
+     *                           shut down, this Bundle contains the data it most recently supplied
+     *                           in onSaveInstanceState(Bundle). Otherwise, it is null.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        // Initialize member variables
         context = getApplicationContext();
         resources = getResources();
         relativeLayout = findViewById(R.id.relativeLayout);
         imageView = findViewById(R.id.imageView);
         imageView.setImageResource(R.drawable.c1map);
-        //Obtain current and next room ids from other activity
+
+        // Obtain current and next room ids from other activity
         start = getIntent().getIntExtra("current", 120);
         end = getIntent().getIntExtra("next", 110);
-        //display info
-        ((TextView)findViewById(R.id.CurrText)).setText("Current:"+ "\n" +"C1"+ start);
-        ((TextView)findViewById(R.id.NextText)).setText("Next:"+ "\n"+"C1"+ end);
 
+        // Display information
+        ((TextView) findViewById(R.id.CurrText)).setText("Current:" + "\n" + "C1" + start);
+        ((TextView) findViewById(R.id.NextText)).setText("Next:" + "\n" + "C1" + end);
 
         path = new Path();
-
     }
 
-
-    public void ShowButton(View v){
-        //access colors from res>values>colors.xml
+    /**
+     * Displays the navigation path and room markers on the map.
+     *
+     * @param v The View that was clicked.
+     */
+    public void ShowButton(View v) {
+        // Access colors from res>values>colors.xml
         int cyan = ContextCompat.getColor(context, R.color.cyan);
         int red = ContextCompat.getColor(context, R.color.red);
         int blue = ContextCompat.getColor(context, R.color.blue);
 
-        //obtain scale value for the map on this device to pass to our path function
-        scaleValValue = imageView.getWidth()/(float)200;
-        C1floor c = new C1floor(scaleValValue,start, end);
+        // Obtain scale value for the map on this device to pass to our path function
+        scaleValValue = imageView.getWidth() / (float) 200;
+        C1floor c = new C1floor(scaleValValue, start, end);
 
-
-        //Initialize display
+        // Initialize display
         Bitmap bitmap = Bitmap.createBitmap(imageView.getWidth(), imageView.getHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
         Paint paint = new Paint();
 
         paint.setColor(cyan);
-        paint.setStrokeWidth(22); //line width
+        paint.setStrokeWidth(22); // Line width
         paint.setAntiAlias(true);
 
-        paint.setStyle(Paint.Style.STROKE);//STOKE for drawLine/drawPath
+        paint.setStyle(Paint.Style.STROKE); // STOKE for drawLine/drawPath
 
-        //Get path
+        // Get path
         path = c.getPath();
 
-        //Display path and room markers
+        // Display path and room markers
         canvas.drawPath(path, paint);
 
-        paint.setStyle(Paint.Style.FILL); //FILL style to make circles
+        paint.setStyle(Paint.Style.FILL); // FILL style to make circles
         paint.setColor(blue);
 
-        canvas.drawCircle(c.getX(start),c.getY(start),35,paint);
+        canvas.drawCircle(c.getX(start), c.getY(start), 35, paint);
         paint.setColor(red);
-        canvas.drawCircle(c.getX(end),c.getY(end),30,paint);
+        canvas.drawCircle(c.getX(end), c.getY(end), 30, paint);
 
-        ImageView map  = findViewById(R.id.pathDisplay);
+        ImageView map = findViewById(R.id.pathDisplay);
 
         map.setImageBitmap(bitmap);
-
     }
 
-    public void GoBackButton(View v){
+    /**
+     * Navigates back to the main activity.
+     *
+     * @param v The View that was clicked.
+     */
+    public void GoBackButton(View v) {
         Intent intent = new Intent(MainActivity2.this, MainActivity.class);
         startActivity(intent);
     }
